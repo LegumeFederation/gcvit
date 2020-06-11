c4c9c2ebfbbce8b73c1ed7f185ca7063fa53e92a
awilkey <awilkey@iastate.edu>
2020-06-11 15:36:29 -0500

    continue ui reorg

---
+++ ui/gcvit/src/Components/ReferenceForm.js
@@ -0,0 +1,22 @@
+/**
+ * Select reference genotype
+ */
+import React from 'react';
+import GenotypeSelector from './GenotypeSelect';
+
+export default class ReferenceForm extends React.Component {
+	render() {
+		return (
+			<fieldset className={'genotype-field'}>
+				<legend> Reference Genotype</legend>
+				<GenotypeSelector
+					datasets={this.props.datasets}
+					setDataset={this.props.setDataset}
+					appendDataset={this.props.appendDataset}
+					genotypes={this.props.genotypes}
+					idx={0}
+				/>
+			</fieldset>
+		);
+	}
+}
