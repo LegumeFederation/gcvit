56873110fad40c186dc161b7dd8b6c7f5cded6f8
awilkey <awilkey@iastate.edu>
2020-07-01 16:11:23 -0500

    update build artifacts for quick start demo

---
+++ ui/gcvit/build/precache-manifest.c5f6b7ae20fef66e146d314f1c5d46f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53f24b9eda82e0e86bca991994c36788",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ecc3efc49f6b1d1ad5d",
+    "url": "/static/css/2.636dc3d6.chunk.css"
+  },
+  {
+    "revision": "adab0cd87399a5afcb7a",
+    "url": "/static/css/main.07ee28f4.chunk.css"
+  },
+  {
+    "revision": "4ecc3efc49f6b1d1ad5d",
+    "url": "/static/js/2.ff9da864.chunk.js"
+  },
+  {
+    "revision": "1039c371838e45c80bd4500e9199a724",
+    "url": "/static/js/2.ff9da864.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adab0cd87399a5afcb7a",
+    "url": "/static/js/main.dabe427e.chunk.js"
+  },
+  {
+    "revision": "9058455821a4c0f9e19b",
+    "url": "/static/js/runtime-main.d00c11c3.js"
+  }
+]);
